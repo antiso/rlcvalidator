42f81e20a2edaab71af799b6cae85f614b65b059
Vladimir Sosnin <antiso@gmail.com>
2013-07-01 02:47:47 +0400

    Script "Create Ack" validator added.

--- rlcvalidator/src/main/java/com/icoegroup/rlcvalidator/ContentCompareValidator.java
+++ rlcvalidator/src/main/java/com/icoegroup/rlcvalidator/ContentCompareValidator.java
@@ -1,11 +1,84 @@
 package com.icoegroup.rlcvalidator;
 
-public class ContentCompareValidator extends XPathValidator {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
+
+public class ContentCompareValidator extends XPathValidator {
+	
+	// Name of XML reference file
+	private String refFileName;
+
+    private static List<String> toLines(String value) {
+        List<String> lines = new LinkedList<String>();
+        String line = "";
+        try {
+                BufferedReader in = new BufferedReader(new StringReader(value));
+                while ((line = in.readLine()) != null) {
+                        lines.add(line);
+                }
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+        return lines;
+}
+
+	
 	@Override
 	public boolean validate() {
-		// TODO Auto-generated method stub
-		return false;
+		NodeList resultNodes = getResultNodes();
+		if (resultNodes.getLength()==0) {
+			info("No nodes found.");
+			return true;
+		}
+
+		List<String> original = toLines(resultNodes.item(0).getNodeValue());
+        List<String> revised  = toLines(getRefValue());
+        Patch patch = DiffUtils.diff(original, revised);
+        boolean result = true;
+        
+        for (Delta delta: patch.getDeltas()) {
+        	result = false;
+            error(delta.toString());
+        }
+
+		
+		return result;
 	}
 
+	public String getRefValue() {
+		ClassPathResource refResource = new ClassPathResource(refFileName);
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(new InputSource(refResource.getInputStream()));
+			return getXpathExpression().evaluate(doc);
+		} catch (Exception e) {
+			error("Erorr", e);
+			throw new ValidatorConfigurationException(e);
+		}
+
+	}
+
+	public String getRefFileName() {
+		return refFileName;
+	}
+
+	public void setRefFileName(String refFileName) {
+		this.refFileName = refFileName;
+	}
 }
---
+++ rlcvalidator/src/test/java/com/icoegroup/rlcvalidator/test/ContentCompareValidatorTest.java
@@ -0,0 +1,37 @@
+package com.icoegroup.rlcvalidator.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.icoegroup.rlcvalidator.ContentCompareValidator;
+import com.icoegroup.rlcvalidator.Context;
+import com.icoegroup.rlcvalidator.Validator;
+import com.icoegroup.rlcvalidator.XsdValidator;
+
+public class ContentCompareValidatorTest {
+
+	private ApplicationContext ctx;
+	private Context validationContext;
+	private ContentCompareValidator contentValidator;
+
+	@Before
+	public void initContext() {
+		ctx = new ClassPathXmlApplicationContext(
+				"classpath:rlcvalidator.xml");
+		validationContext = ctx.getBean(Context.class);
+		contentValidator = (ContentCompareValidator) ctx.getBean("CreateAckScriptValidator");
+	}
+
+	@Test
+	public void test() {
+		String rlcFileName = "src/test/resources/testfiles/routes.xml";
+		validationContext.setRlcFileName(rlcFileName);
+		assertTrue(contentValidator.validate());
+	}
+
+}
